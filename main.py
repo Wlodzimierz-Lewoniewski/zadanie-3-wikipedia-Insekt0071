e9ab9e72c7e116b4fbba67ce3de2d8dc9ccb1b10
Insekt0071 <128573916+Insekt0071@users.noreply.github.com>
2024-10-28 17:25:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import re
+import requests
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+url = "https://pl.wikipedia.org/wiki/Pomoc:Przestrze%C5%84_nazw"
+response = requests.get(url)
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+if response.status_code == 200:
+    html_content = response.text
+    print("Kod HTML strony pomocniczej pobrany pomyślnie.")
+else:
+    print("Błąd podczas pobierania strony.")
+
+#wzorzec do patternów
+pattern = r'href="(/wiki/[^":#]+)"'
+
+# Znalezienie wszystkich dopasowań wzorca w kodzie HTML
+links = re.findall(pattern, html_content)
+
+# Wyświetlenie pierwszych pięciu dopasowań
+print("Znalezione odnośniki wewnętrzne:")
+for link in links[:5]:
+    print("https://pl.wikipedia.org" + link)
+
+# Wzorzec dla adresów URL obrazków
+image_pattern = r'src="(//upload\.wikimedia\.org[^"]+\.(jpg|jpeg|png|svg))"'
+
+# Znalezienie dopasowań dla obrazów
+images = re.findall(image_pattern, html_content)
+
+# Wyświetlenie pierwszych trzech URL obrazków
+print("Znalezione URL obrazków:")
+for img in images[:3]:
+    print("https:" + img[0])  # Dodajemy "https:" na początku URL
