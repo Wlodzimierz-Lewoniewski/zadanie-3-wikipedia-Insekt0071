5154f1e98b39ce688cd2afcf98ab24d98c6d30cc
Insekt0071 <128573916+Insekt0071@users.noreply.github.com>
2024-10-29 09:59:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,48 +2,47 @@ import re
 import requests
 from urllib.parse import quote
 
-# Przyjęcie hasła od użytkownika
-query = input("Podaj hasło do wyszukania na Wikipedii: ")
-url = f"https://pl.wikipedia.org/wiki/{quote(query)}"
+def get_wiki_content(query):
+    #wwyszukiwanie
+    url = f"https://pl.wikipedia.org/wiki/{quote(query)}"
+    response = requests.get(url)
 
-# Pobieranie strony
-response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise Exception("blad podczas pobierania strony.")
 
-if response.status_code == 200:
-    html_content = response.text
-    print(f"Kod HTML strony '{query}' pobrany pomyślnie.\n")
-else:
-    print("Błąd podczas pobierania strony.")
-    exit()
+def extract_internal_links(html_content):
+    pattern = r'href="(/wiki/[^":#]+)"'
+    links = re.findall(pattern, html_content)
+    formatted_links = [link.split('/')[-1].replace('_', ' ') for link in links]
+    return " | ".join(formatted_links[:5])
 
-# Wzorzec dla odnośników wewnętrznych
-pattern = r'href="(/wiki/[^":#]+)"'
-links = re.findall(pattern, html_content)
+def extract_images(html_content):
+    image_pattern = r'src="(//upload\.wikimedia\.org[^"]+\.(jpg|jpeg|png|svg))"'
+    images = re.findall(image_pattern, html_content)
+    formatted_images = ["https:" + img[0] for img in images[:3]]
+    return " | ".join(formatted_images)
 
-print("Znalezione odnośniki wewnętrzne:")
-for link in links[:5]:
-    print("https://pl.wikipedia.org" + link)
+def extract_external_links(html_content):
+    external_link_pattern = r'href="(https?://[^"]+)"'
+    external_links = re.findall(external_link_pattern, html_content)
+    return " | ".join(external_links[:3])
 
-# Wzorzec dla adresów URL obrazków
-image_pattern = r'src="(//upload\.wikimedia\.org[^"]+\.(jpg|jpeg|png|svg))"'
-images = re.findall(image_pattern, html_content)
+def extract_categories(html_content):
+    category_pattern = r'href="/wiki/Kategoria:[^":#]+" title="([^"]+)"'
+    categories = re.findall(category_pattern, html_content)
+    return " | ".join(categories[:3])
 
-print("\nZnalezione URL obrazków:")
-for img in images[:3]:
-    print("https:" + img[0])  # Dodajemy "https:" na początku URL
+#pobieranie kou html
+html_content = get_wiki_content("Miasta na prawach powiatu")
 
-# Wzorzec dla URL źródeł zewnętrznych
-external_link_pattern = r'href="(https?://[^"]+)"'
-external_links = re.findall(external_link_pattern, html_content)
+internal_links = extract_internal_links(html_content)
+images = extract_images(html_content)
+external_links = extract_external_links(html_content)
+categories = extract_categories(html_content)
 
-print("\nZnalezione URL źródeł zewnętrznych:")
-for link in external_links[:3]:
-    print(link)
-
-# Wzorzec dla kategorii
-category_pattern = r'href="/wiki/Kategoria:[^":#]+" title="([^"]+)"'
-categories = re.findall(category_pattern, html_content)
-
-print("\nZnalezione kategorie artykułu:")
-for category in categories[:3]:
-    print(category)
+print(internal_links, images, external_links, categories)
+#print(images)
+#print(external_links)
+#print(categories)
