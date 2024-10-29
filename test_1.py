9132da0b9dcee695ddfded101528c3bcfb2e211c
Insekt0071 <83713@student.ue.poznan.pl>
2024-10-29 09:58:32 +0100

    test 5 lub 6

---
+++ test_1.py
@@ -0,0 +1,49 @@
+import re
+import requests
+from urllib.parse import quote
+
+# Przyjęcie hasła od użytkownika
+query = "Miasto_na_prawach_powiatu_w_Polsce"
+url = f"https://pl.wikipedia.org/wiki/Miasto_na_prawach_powiatu"
+
+# Pobieranie strony
+response = requests.get(url)
+
+if response.status_code == 200:
+    html_content = response.text
+    print(f"Kod HTML strony '{query}' pobrany pomyślnie.\n")
+else:
+    print("Błąd podczas pobierania strony.")
+    exit()
+
+# Wzorzec dla odnośników wewnętrznych
+pattern = r'href="(/wiki/[^":#]+)"'
+links = re.findall(pattern, html_content)
+
+print("Znalezione odnośniki wewnętrzne:")
+for link in links[:5]:
+    print("https://pl.wikipedia.org" + link)
+
+# Wzorzec dla adresów URL obrazków
+image_pattern = r'src="(//upload\.wikimedia\.org[^"]+\.(jpg|jpeg|png|svg))"'
+images = re.findall(image_pattern, html_content)
+
+print("\nZnalezione URL obrazków:")
+for img in images[:3]:
+    print("https:" + img[0])  # Dodajemy "https:" na początku URL
+
+# Wzorzec dla URL źródeł zewnętrznych
+external_link_pattern = r'href="(https?://[^"]+)"'
+external_links = re.findall(external_link_pattern, html_content)
+
+print("\nZnalezione URL źródeł zewnętrznych:")
+for link in external_links[:3]:
+    print(link)
+
+# Wzorzec dla kategorii
+category_pattern = r'href="/wiki/Kategoria:[^":#]+" title="([^"]+)"'
+categories = re.findall(category_pattern, html_content)
+
+print("\nZnalezione kategorie artykułu:")
+for category in categories[:3]:
+    print(category)
