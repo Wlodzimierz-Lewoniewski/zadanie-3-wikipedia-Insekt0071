fbe67f3f5377708ace016f6ef7e92ca1fcdf5c1c
Insekt0071 <83713@student.ue.poznan.pl>
2024-11-04 15:19:41 +0100

    wraaaaa

--- main.py
+++ main.py
@@ -2,45 +2,50 @@ import re
 import requests
 from urllib.parse import quote
 
-def get_wiki_content(query):
-    #wwyszukiwanie
-    url = f"https://pl.wikipedia.org/wiki/{quote(query)}"
+def get_html_content(url):
     response = requests.get(url)
-
     if response.status_code == 200:
         return response.text
     else:
-        raise Exception("blad podczas pobierania strony.")
+        raise Exception("Błąd podczas pobierania strony.")
 
 def extract_internal_links(html_content):
     pattern = r'href="(/wiki/[^":#]+)"'
     links = re.findall(pattern, html_content)
-    formatted_links = [link.split('/')[-1].replace('_', ' ') for link in links]
-    return " | ".join(formatted_links[:5])
+    article_links = [link for link in links if not link.startswith('/wiki/Kategoria:')]
+    return article_links[:2]
 
-def extract_images(html_content):
+def extract_data_from_article(url):
+    html_content = get_html_content(url)
+
+    internal_links_pattern = r'href="(/wiki/[^":#]+)"'
+    internal_links = re.findall(internal_links_pattern, html_content)
+    formatted_internal_links = " | ".join(link.split('/')[-1].replace('_', ' ') for link in internal_links[:5])
+
     image_pattern = r'src="(//upload\.wikimedia\.org[^"]+\.(jpg|jpeg|png|svg))"'
     images = re.findall(image_pattern, html_content)
-    formatted_images = ["https:" + img[0] for img in images[:3]]
-    return " | ".join(formatted_images)
+    formatted_images = " | ".join("https:" + img[0] for img in images[:3])
 
-def extract_external_links(html_content):
     external_link_pattern = r'href="(https?://[^"]+)"'
     external_links = re.findall(external_link_pattern, html_content)
-    return " | ".join(external_links[:3])
+    formatted_external_links = " | ".join(external_links[:3])
 
-def extract_categories(html_content):
     category_pattern = r'href="/wiki/Kategoria:[^":#]+" title="([^"]+)"'
     categories = re.findall(category_pattern, html_content)
-    return " | ".join(categories[:3])
+    formatted_categories = " | ".join(categories[:3])
 
-#pobieranie kou html
-a = input()
-html_content = get_wiki_content(a)
+    return formatted_internal_links, formatted_images, formatted_external_links, formatted_categories
 
-internal_links = extract_internal_links(html_content)
-images = extract_images(html_content)
-external_links = extract_external_links(html_content)
-categories = extract_categories(html_content)
+category_query = input()
+category_url = f"https://pl.wikipedia.org/wiki/{quote(category_query)}"
+category_html_content = get_html_content(category_url)
 
-print(internal_links, images, external_links, categories)
+article_links = extract_internal_links(category_html_content)
+
+output = ""
+for link in article_links:
+    article_url = f"https://pl.wikipedia.org{link}"
+    internal_links, images, external_links, categories = extract_data_from_article(article_url)
+    output += f"{internal_links}\n{images}\n{external_links}\n{categories}\n"
+
+print(output.strip())
